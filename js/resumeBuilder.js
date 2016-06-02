db5a707104aa6d9881861a04ecb0b148a8a9578c
Camron Moore <camronbmoore@gmail.com>
2016-06-02 15:39:55 -0600

    Added map and encapcilation

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,57 +1,52 @@
 var bio = {
     "name": "Camron Moore",
     "role": "Front-End Web Developer",
-    "contactInfo": {
+    "contacts": {
         "email": "camronbmoore@gmail.com",
         "phone": "720-263-0276",
         "github": "https://github.com/cbm360",
-        "linkedin": "https://www.linkedin.com/in/camronmoore"
+        "linkedin": "https://www.linkedin.com/in/camronmoore",
+        "location": "Denver, CO"
     },
     "picture": "images/headshot.jpg",
     "welcomeMessage": "Life Long Learner, Team Leader, Front-End Web Developer, Scrum Master",
-    "skills": [" JavaScript", " jQuery", " NodeJS", " Aurelia", " KnockoutJS", " Leaflet", " Ruby on Rails", " GIT", " Visual Studio Online", " Twitter Bootstrap", " HTML5", " CSS3", " T-SQL", " MongoDB"],
-    "location": "Denver, CO"
+    "skills": [" JavaScript", " jQuery", " NodeJS", " Aurelia", " KnockoutJS", " Leaflet", " Ruby on Rails", " Twitter Bootstrap", " HTML5", " CSS3"]
 };
 
 var work = {
-    "positions": [
+    "jobs": [
         {
             "employer": "CAP Logistics",
             "title": "Scrum Master, Front-End Web Developer",
-            "city": "Denver",
-            "state": "CO",
+            "location": "Denver, CO",
             "dates": "July 2013 - Current",
             "description": "Responsible for leading all Scrum ceremonies, and leading all front-end web development."
         },
         {
             "employer": "CAP Logistics",
             "title": "Business Analyst",
-            "city": "Denver",
-            "state": "CO",
+            "location": "Denver, CO",
             "dates": "April 2010 - July 2013",
             "description": "Write software requirements, and design and build actionable business intelligence."
         },
         {
             "employer": "MECR Ventures, LLC",
             "title": "CO-Founder, Business Consultant",
-            "city": "Denver",
-            "state": "CO",
+            "location": "Denver, CO",
             "dates": "August 2008 - January 2011",
             "description": "Consult with businesses on business intelligence, recruiting, and business process."
         },
         {
             "employer": "Sigma Think Tank",
             "title": "CO-Founder",
-            "city": "Denver",
-            "state": "CO",
+            "location": "Denver, CO",
             "dates": "August 2008 - December 2010",
             "description": "Non-profit vendor free networking group."
         },
         {
             "employer": "Tradition Golf Club",
             "title": "Assistant General Manager",
-            "city": "La Quinta",
-            "state": "CA",
+            "location": "La Quinta, CA",
             "dates": "October 2005 - July 2008",
             "description": "Oversee all club operations."
         }
@@ -63,12 +58,14 @@ var education = {
         {
             "name": "University of Northern Colorado",
             "degree": "Bachelors Degree Communication",
-            "years": "1998 - 2002"
+            "years": "1998 - 2002",
+            "location": "Greeley, CO"
         },
         {
             "name": "University of Northern Colorado",
             "degree": "Bachelors Degree Business Administration and Management",
-            "years": "1998 - 2002"
+            "years": "1998 - 2002",
+            "location": "Greeley, CO"
         }
     ],
     "Online Courses": [
@@ -124,20 +121,21 @@ var projects = {
             "images": "images/financetracker.jpg"
         }
     ]
-}
+};
 
-function displayBio(){
+
+bio.display = function() {
     var formattedPic = HTMLbioPic.replace("%data%", bio.picture);
     var formattedName = HTMLheaderName.replace("%data%", bio.name);
     var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-    var formattedContactPhone = HTMLmobile.replace("%data%", bio.contactInfo.phone);
-    var formattedContactEmail = HTMLemail.replace("%data%", bio.contactInfo.email);
-    var formattedContactGithub = HTMLgithub.replace("%data%", bio.contactInfo.github);
-    var formattedLocation = HTMLlocation.replace("%data%", bio.location);
+    var formattedContactPhone = HTMLmobile.replace("%data%", bio.contacts.phone);
+    var formattedContactEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedContactGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
     var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
     var formattedSkills = HTMLskills.replace("%data%", bio.skills);
 
-    $("#header").prepend(formattedPic);
+    $("#header").append(formattedPic);
     $("#header").prepend(formattedRole);
     $("#header").prepend(formattedName);
     $("#topContacts").append(formattedContactPhone);
@@ -145,29 +143,7 @@ function displayBio(){
     $("#topContacts").append(formattedContactGithub);
     $("#topContacts").append(formattedLocation);
     $("#header").append(formattedWelcome);
-}
-displayBio();
 
-function displayWork(){
-    if (work.positions.length > 0) {
-        for (position in work.positions) {
-            var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.positions[position].employer);
-            var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.positions[position].title);
-            var formattedEmployerTitle = formattedWorkEmployer + formattedWorkTitle;
-            $("#workExperience").append(HTMLworkStart);
-            $(".work-entry:last").append(formattedEmployerTitle);
-
-            var formattedDates = HTMLworkDates.replace("%data%", work.positions[position].dates);
-            $(".work-entry:last").append(formattedDates);
-
-            var formattedDescripiton = HTMLworkDescription.replace("%data%", work.positions[position].description);
-            $(".work-entry:last").append(formattedDescripiton);
-        }
-    }
-}
-displayWork();
-
-function displaySkills(){
     if (bio.skills.length > 0) {
         $("#header").append(HTMLskillsStart);
 
@@ -176,7 +152,53 @@ function displaySkills(){
         }
     }
 }
-displaySkills();
+bio.display();
+
+projects.display = function() {
+    if (projects.projects.length > 0) {
+        for (project in projects.projects) {
+            $("#projects").append(HTMLprojectStart);
+
+            var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+            $(".project-entry:last").append(formattedProjectTitle);
+
+            var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+            $(".project-entry:last").append(formattedProjectDates);
+
+            var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+            $(".project-entry:last").append(formattedProjectDescription);
+
+            var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[project].images);
+            $(".project-entry:last").append(formattedProjectImage);
+        }
+    }
+}
+projects.display();
+
+work.display = function() {
+    if (work.jobs.length > 0) {
+        for (position in work.jobs) {
+            $("#workExperience").append(HTMLworkStart);
+
+            var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.jobs[position].employer);
+            var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[position].title);
+            var formattedEmployerTitle = formattedWorkEmployer + formattedWorkTitle;
+            $(".work-entry:last").append(formattedEmployerTitle);
+
+            var formattedDates = HTMLworkDates.replace("%data%", work.jobs[position].dates);
+            $(".work-entry:last").append(formattedDates);
+
+            var formattedDescripiton = HTMLworkDescription.replace("%data%", work.jobs[position].description);
+            $(".work-entry:last").append(formattedDescripiton);
+        }
+    }
+}
+work.display();
+
+function mapDisplay() {
+    $("#mapDiv").append(googleMap);
+}
+mapDisplay();
 
 function recordClick(){
     $(document).click(function(loc) {
@@ -186,6 +208,3 @@ function recordClick(){
     });
 }
 recordClick();
-
-
-
